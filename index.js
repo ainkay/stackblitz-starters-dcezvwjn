cc5082093c875b94bb47643062b16b6525db7197
ainkay <ain.kay@kalvium.community>
2025-01-08 09:42:23 +0530

    added credentials in the env file and edited the index.js to connect to database succesfully

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+// index.js
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./schema.js'); 
+dotenv.config();
 
 const app = express();
-const port = 3010;
+app.use(express.json()); 
 
-app.use(express.static('static'));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.error('Error connecting to database:', err));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = new User(userData);
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res
+        .status(400)
+        .json({ message: 'Validation error', details: error.message });
+    } else {
+      res.status(500).json({ message: 'Server error', details: error.message });
+    }
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
